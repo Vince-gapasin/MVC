3553dd40b134fc9c20bd56d257aeb3a9302c4074
ChristianBacani <christianbac554@gmail.com>
2026-03-19 17:39:27 +0800

    Organized

--- Program.cs
+++ Program.cs
@@ -15,11 +15,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
